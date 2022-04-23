927e73e98ea08703120bb7da752875a90177bb35
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-04-23 17:12:05 +0530

    project#3

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -14,16 +15,32 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     console.log(i);
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+// }
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
 
-for (let i = 0; i < btnsOpenModal.length; i++)
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
+  console.log(e.key);
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
