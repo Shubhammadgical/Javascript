645ad9662478be64c3b19e11ca6366c28b9dbfb7
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-05-02 13:42:55 +0530

    updated0n2

---
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/Filter,reduce&Find.js
@@ -0,0 +1,89 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposit = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposit);
+
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
+
+const balance = movements.reduce((acc, curr, i, arr) => {
+  console.log(`Iteration ${i} : ${acc}`);
+  return acc + curr;
+}, 0);
+console.log(balance);
+
+//Maximum
+const max = movements.reduce(
+  (acc, curr) => (curr < acc ? acc : curr),
+  movements[0]
+);
+console.log(max);
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const dogsGreaterthan18 = humanAges.filter(dog => dog >= 18);
+  const avg =
+    dogsGreaterthan18.reduce((acc, curr) => acc + curr, 0) /
+    dogsGreaterthan18.length;
+};
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+//arrow fn
+const calcAverageHumanAge2 = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(dog => dog >= 18)
+    .reduce((acc, curr, i, arr) => acc + curr / arr.length, 0);
+
+const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
+//PIPELINE
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, curr) => acc + curr, 0);
+console.log(totalDepositsUSD);
+
+//find
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const firstwithdrawal = movements.find(mov => mov < 0);
+console.log(firstwithdrawal);
+
+const account = accounts.find(acc => acc.owner === 'Sarah Smith');
+console.log(account);
